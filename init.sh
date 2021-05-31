557dccf71212ccfc5752726d7d0cd27d5611ca8d
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-05-31 15:28:22 +0900

    Add barrier

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 sudo pacman -Syyu
-sudo pacman -S yay arduino arduino-avr-core
-yay -S fd ripgrep starship exa fzf fish neovim pyenv poetry nodejs npm cheese
+sudo pacman -S yay arduino arduino-avr-core cheese barrier
+sudo pacman -S --needed base-devel openssl zlib xz
+yay -S fd ripgrep starship exa fzf fish neovim pyenv poetry nodejs npm
 sudo npm install n -g
 sudo n stable
 yay -Rs nodejs npm
-sudo pacman -S --needed base-devel openssl zlib xz
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 rm ./installer.sh
