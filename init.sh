ea38cfeacc0f2c3dd066a89e988f8f741696cfa2
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2023-06-30 13:34:34 +0900

    Move from manjoro to ubuntu so rewrite `init.sh`.

--- init.sh
+++ init.sh
@@ -1,16 +1,49 @@
-sudo pacman -Syyu --noconfirm
-sudo pacman -S yay arduino arduino-avr-core cheese barrier xdg-user-dirs-gtk --noconfirm
-LC_ALL=C xdg-user-dirs-gtk-update
-sudo pacman -S --needed base-devel openssl zlib xz --noconfirm
-yay -S fd ripgrep starship exa fzf fish neovim pyenv poetry nodejs npm --noconfirm
-sudo npm install n -g
-sudo n stable
-yay -Rs nodejs npm --noconfirm
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ~/.cache/dein
-rm ./installer.sh
-pyenv install 3.7.10
+# Below commented out lines must be run before execute `init.sh`.
+# sudo apt update && sudo apt upgrade -y
+# sudo apt install git -y
+# git clone [https://github.com/7cm-diameter/rspb] ~/.rspb
+# cd .rspb && sh raspberry-pi_setup.sh
+# You also download `Hack Nerd-fonts` from https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/Hack.
+# Downloaded patches must be in `$HOME/.local/share/fonts`.
+
+sudo snap install neovim --classic
+sudo snap install chezmoi --classic
+sudo apt install -y fish fzf build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev nodejs npm portaudio19-dev cmake
+
+# Enter fish.
+fish
+
+# Install python and other utilities for python.
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+set -Ux $PYENV_ROOT/.pyenv
+fish_add_path $PYENV_ROOT/bin
+pyenv install 3.10.12
+pyenv global 3.10.12
+curl -sSL https://install.python-poetry.org | python3 -
+fish_add_path $HOME/.local/bin
 poetry config virtualenvs.in-project true
-arduino --version
-sudo usermod -aG uucp $USER
-git clone https://github.com/arduino-libraries/Servo ~/Arduino/libraries/Servo
+
+# Install cargo and install binaries through cargo.
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+set -Ux $RUST_ROOT $HOME/.cargo
+fish_add_path $RUST_ROOT/bin
+cargo install exa fd-find ripgrep starship
+
+# Install arduino-cli and other dependecies for `pino`.
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=~/.local/bin sh
+arduino-cli core install arduino:avr
+arduino-cli lib install servo
+sudo usermod -a -G dialout $USER
+
+# Install nodejs
+sudo npm -g install n
+sudo n stable
+sudo apt purge nodejs npm
+sudo apt autotemove -y
+sudo npm -g install yarn
+
+# Apply config
+chezmoi init https://github.com/7cm-diameter/dotfiles
+chezmoi apply
+
+sudo chsh -s /usr/bin/fish
--- nvim/ftplugin/r.vim
+++
@@ -1,4 +0,0 @@
-setlocal shiftwidth=2
-setlocal tabstop=2
-inoremap <buffer> >> <Esc>:normal! a%>%<CR>a
-setlocal number
